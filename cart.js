db2866102a58a9cf2623b1f1a6ab19fb3104c737
Valet12 <torovaleria1803@gmail.com>
2025-07-04 17:27:05 +0200

    CV

--- cart.js
+++
@@ -1,6 +0,0 @@
-// Placeholder for cart logic
-document.querySelectorAll('.add-to-cart').forEach(button => {
-  button.addEventListener('click', () => {
-    alert('This item has been added to your cart!');
-  });
-});
